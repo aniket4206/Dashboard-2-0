b25c41662b5de24ebeeee18e2f6c01ee2074adc9
Divesh Jadhav <diveshvjadhav@gmail.com>
2021-09-18 10:21:53 +0530

    Update TopSection.js

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -2,7 +2,7 @@ import TopSectionOptions from "./TopSectionOptions"
 
 function TopSection() {
     return (
-        <div className="p-2 xl:space-x-4 flex">
+        <div className="p-2 xl:space-x-4 flex sm:flex-col sm:space-y-2">
             <TopSectionOptions title="Total Applications" color="text-purple-600" color2="#7F00FF" color3="hover:bg-purple-400" total="7956" percentage="3.59" percentage2="70"  />
             <TopSectionOptions title="Shortlisted Candidates" color="text-blue-500" color2="#1F51FF"
             color3="hover:bg-blue-400" total="4658" percentage="06" percentage2="60"  />
